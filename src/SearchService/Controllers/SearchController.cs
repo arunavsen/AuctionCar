968646d149ade8493dd47a238af3ecd955c784cf
arunavsen <arunavsen96@gmail.com>
2024-07-10 23:53:43 +0600

    Add SearchItem action

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -14,30 +14,40 @@ public class SearchController : ControllerBase
     {
         var query = DB.PagedSearch<Item, Item>();
 
-        // Adding a default sort order to avoid the "Paging without sorting" error
-        query.Sort(x => x.Ascending(a => a.CreatedAt));
-
         if (!string.IsNullOrEmpty(searchParams.SearchTerm))
         {
             query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
 
-        //query = searchParams.FilterBy switch
-        //{
-        //    "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-        //    "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
-        //        && x.AuctionEnd > DateTime.UtcNow),
-        //    _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
-        //};
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
 
+        query = searchParams.FilterBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
 
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
 
         query.PageNumber(searchParams.PageNumber);
         query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
-
-        Console.WriteLine($"Total Count: {result.TotalCount}, Page Count: {result.PageCount}, Results Count: {result.Results.Count}");
 
         return Ok(new
         {
@@ -46,8 +56,4 @@ public class SearchController : ControllerBase
             totalCount = result.TotalCount
         });
     }
-
-
-
-
 }
